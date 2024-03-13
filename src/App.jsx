0c46d499af553b0382f5dd363b0f1fd99ccf4cd3
bmartin86 <iMuy11pMpk>
2024-03-13 12:48:00 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1 style={{color: 'white'}}>Vite + React</h1>
+      <h1 style={{color: 'white'}}>Vite + React</h1> */}
       <Newsletter />
     </>
   )
